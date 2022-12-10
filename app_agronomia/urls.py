09dd15a9f7731906f41942b3e5a04bbb23164519
Yeltsin <yeltsinbaquedano@gmail.com>
2022-12-09 21:18:03 -0600

    Se agrego detalle de galeria

--- app_agronomia/urls.py
+++ app_agronomia/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
     path("post_enfermedad", post_enfermedad, name="post_enfermedad"),
     path("post_asignacion", post_asignacion, name="post_asignacion"),
     path("post_cbo_enfermedades", post_cbo_enfermedades, name="post_cbo_enfermedades"),
-    path("plantas_entrenadas", plantas_entrenadas, name="plantas_entrenadas")
-    
+    path("plantas_entrenadas", plantas_entrenadas, name="plantas_entrenadas"),
+    path('detalle_galeria/<int:id_planta>/', detalle_galeria, name='detalle_galeria')
 ]
